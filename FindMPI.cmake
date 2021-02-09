80c42699121c94e00cd11a75c2b7781fcbfaac70
Julien EMMANUEL <webcretaire@julien-emmanuel.com>
2021-02-09 16:17:41 +0100

    Add definitions for LULESH

--- FindMPI.cmake
+++ FindMPI.cmake
@@ -10,8 +10,16 @@ find_library(MPI_LIBRARY
 	NAMES mpi
 	PATHS ${MPI_PATH}/lib /opt/ompi_bull/lib
 	)
+set(MPI_C_INCLUDE_PATH ${MPI_INCLUDE_DIR})
+set(MPI_CXX_INCLUDE_PATH ${MPI_INCLUDE_DIR})
+set(MPI_C_LIBRARIES ${MPI_LIBRARY})
+set(MPI_CXX_LIBRARIES ${MPI_LIBRARY})
 mark_as_advanced(MPI_INCLUDE_DIR)
+mark_as_advanced(MPI_C_INCLUDE_PATH)
+mark_as_advanced(MPI_CXX_INCLUDE_PATH)
 mark_as_advanced(MPI_LIBRARY)
+mark_as_advanced(MPI_C_LIBRARIES)
+mark_as_advanced(MPI_CXX_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MPI
